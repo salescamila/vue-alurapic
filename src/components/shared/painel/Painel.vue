b51de4a251b2878a8075fb46835afd3d780d46d8
Camila <salescamila1@gmail.com>
2019-10-17 22:36:30 -0400

    estilos e escopo

--- src/components/shared/painel/Painel.vue
+++ src/components/shared/painel/Painel.vue
@@ -12,7 +12,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
   /* estilo do painel */
    .painel {
     padding: 0 auto;
@@ -33,6 +33,10 @@ export default {
     margin: 0 0 15px 0;
     padding: 10px;
     text-transform: uppercase;
+  }
+
+  * {
+    box-shadow: 5px 5px 5px black;
   }
 
 </style>
