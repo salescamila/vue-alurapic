0c783f34c7dcd93fdbf99e1baf34d0d30b6f4566
Camila <salescamila1@gmail.com>
2019-10-28 21:18:34 -0400

    o menu da aplicação

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,13 +1,28 @@
 <template>
   <div class="corpo">
+    <nav>
+      <ul class="list-menu">
+        <li class="list-menu-item" v-for="route in routes" :key="route">
+          <router-link :to="route.path ? route.path : '/'">
+            {{ route.titulo }}
+          </router-link>
+        </li>
+      </ul>
+    </nav>
+
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 
+import { routes } from './routes';
 export default {
-
+  data() {
+    return{
+      routes
+    }
+  }
 }
 </script>
 
@@ -16,5 +31,9 @@ export default {
     font-family: Helvetica, sans-serif;
     width: 96%;
     margin: 0 auto;
+  }
+  .list-menu-item{
+    display:inline-block;
+    margin-right: 2%;
   }
 </style>
